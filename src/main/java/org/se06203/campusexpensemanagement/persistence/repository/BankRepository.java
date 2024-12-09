d97f272c47713a2041325bd1d5fe8faeb9d6de0c
do.thang <100296543+DTD2604@users.noreply.github.com>
2024-12-09 16:24:38 +0700

    fix api

--- src/main/java/org/se06203/campusexpensemanagement/persistence/repository/BankRepository.java
+++ src/main/java/org/se06203/campusexpensemanagement/persistence/repository/BankRepository.java
@@ -2,12 +2,11 @@ package org.se06203.campusexpensemanagement.persistence.repository;
 
 import org.se06203.campusexpensemanagement.persistence.entity.Banks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BankRepository extends JpaRepository<Banks, Long> {
-
-    List<Banks> getAllByUserId(Long userId);
 }
